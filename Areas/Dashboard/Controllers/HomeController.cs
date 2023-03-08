2dfc846c5d1966b8e8103a06dc17accb10dd4753
tidvn <tiendung0325@gmail.com>
2023-03-08 09:57:54 +0700

    Add project files.

---
+++ Areas/Dashboard/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DStore.Areas.Dashboard.Controllers
+{
+    [Area("Dashboard")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+    
---
+++ Program.cs
@@ -0,0 +1,36 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapAreaControllerRoute(
+    name: "default",
+    areaName: "Store",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapAreaControllerRoute(
+            name: "Dashboard",
+            areaName: "Dashboard",
+            pattern: "Dashboard/{controller=Home}/{action=Index}/{id?}");
+
+
+
+
+app.Run();
